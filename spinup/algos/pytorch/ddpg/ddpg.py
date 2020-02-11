9aac46ab5fce4f01432f6754ef3e0684fa1dbdeb
Alan Fontoura <fontoura.alr@gmail.com>
2020-02-11 15:22:37 -0300

    Added ALMEnv and changed related code entries

--- spinup/algos/pytorch/ddpg/ddpg.py
+++ spinup/algos/pytorch/ddpg/ddpg.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 import numpy as np
+import pandas as pd
 import torch
 from torch.optim import Adam
 import gym
@@ -39,13 +40,21 @@ class ReplayBuffer:
                      done=self.done_buf[idxs])
         return {k: torch.as_tensor(v, dtype=torch.float32) for k,v in batch.items()}
 
-
-
-def ddpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0, 
-         steps_per_epoch=4000, epochs=100, replay_size=int(1e6), gamma=0.99, 
-         polyak=0.995, pi_lr=1e-3, q_lr=1e-3, batch_size=100, start_steps=10000, 
-         update_after=1000, update_every=50, act_noise=0.1, num_test_episodes=10, 
-         max_ep_len=1000, logger_kwargs=dict(), save_freq=1):
+"""
+The following parameters have been changed:
+env_fn: default value set to core.ALMEnv
+epochs: default value changed from 100 to 300
+act_noise: default value changed from .1 to .01
+time_horizon: added parameter, with default value 80
+discount_rate: added parameter, with default value .06
+"""
+def ddpg(env_fn = core.ALMEnv, actor_critic = core.MLPActorCritic,
+         ac_kwargs = dict(), seed = 0, steps_per_epoch = 4000, epochs = 300,
+         replay_size = int(1e6), gamma = 0.99, polyak=0.995, pi_lr = 1e-3,
+         q_lr = 1e-3, batch_size = 100, start_steps = 10000, update_after = 1000,
+         update_every = 50, act_noise = .01, num_test_episodes = 10,
+         max_ep_len = 1000, logger_kwargs = dict(), save_freq = 1,
+         time_horizon = 80, discount_rate = .06):
     """
     Deep Deterministic Policy Gradient (DDPG)
 
@@ -53,17 +62,18 @@ def ddpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
     Args:
         env_fn : A function which creates a copy of the environment.
             The environment must satisfy the OpenAI Gym API.
+            In this version, the default environment is 'ALMEnv'
 
-        actor_critic: The constructor method for a PyTorch Module with an ``act`` 
+        actor_critic: The constructor method for a PyTorch Module with an ``act``
             method, a ``pi`` module, and a ``q`` module. The ``act`` method and
             ``pi`` module should accept batches of observations as inputs,
-            and ``q`` should accept a batch of observations and a batch of 
+            and ``q`` should accept a batch of observations and a batch of
             actions as inputs. When called, these should return:
 
             ===========  ================  ======================================
             Call         Output Shape      Description
             ===========  ================  ======================================
-            ``act``      (batch, act_dim)  | Numpy array of actions for each 
+            ``act``      (batch, act_dim)  | Numpy array of actions for each
                                            | observation.
             ``pi``       (batch, act_dim)  | Tensor containing actions from policy
                                            | given observations.
@@ -73,12 +83,12 @@ def ddpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
                                            | flatten this!)
             ===========  ================  ======================================
 
-        ac_kwargs (dict): Any kwargs appropriate for the ActorCritic object 
+        ac_kwargs (dict): Any kwargs appropriate for the ActorCritic object
             you provided to DDPG.
 
         seed (int): Seed for random number generators.
 
-        steps_per_epoch (int): Number of steps of interaction (state-action pairs) 
+        steps_per_epoch (int): Number of steps of interaction (state-action pairs)
             for the agent and the environment in each epoch.
 
         epochs (int): Number of epochs to run and train agent.
@@ -87,14 +97,14 @@ def ddpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
 
         gamma (float): Discount factor. (Always between 0 and 1.)
 
-        polyak (float): Interpolation factor in polyak averaging for target 
-            networks. Target networks are updated towards main networks 
+        polyak (float): Interpolation factor in polyak averaging for target
+            networks. Target networks are updated towards main networks
             according to:
 
-            .. math:: \\theta_{\\text{targ}} \\leftarrow 
+            .. math:: \\theta_{\\text{targ}} \\leftarrow
                 \\rho \\theta_{\\text{targ}} + (1-\\rho) \\theta
 
-            where :math:`\\rho` is polyak. (Always between 0 and 1, usually 
+            where :math:`\\rho` is polyak. (Always between 0 and 1, usually
             close to 1.)
 
         pi_lr (float): Learning rate for policy.
@@ -111,11 +121,11 @@ def ddpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
             is full enough for useful updates.
 
         update_every (int): Number of env interactions that should elapse
-            between gradient descent updates. Note: Regardless of how long 
-            you wait between updates, the ratio of env steps to gradient steps 
+            between gradient descent updates. Note: Regardless of how long
+            you wait between updates, the ratio of env steps to gradient steps
             is locked to 1.
 
-        act_noise (float): Stddev for Gaussian exploration noise added to 
+        act_noise (float): Stddev for Gaussian exploration noise added to
             policy at training time. (At test time, no noise is added.)
 
         num_test_episodes (int): Number of episodes to test the deterministic
@@ -136,7 +146,9 @@ def ddpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
     torch.manual_seed(seed)
     np.random.seed(seed)
 
-    env, test_env = env_fn(), env_fn()
+    # env, test_env = env_fn(), env_fn() original OpenAI SpinningUp entry
+    env = env_fn(T = time_horizon, rate = discount_rate) # Added by the author
+    test_env = env_fn(T = time_horizon, rate = discount_rate) # Added by the author
     obs_dim = env.observation_space.shape
     act_dim = env.action_space.shape[0]
 
@@ -197,7 +209,7 @@ def ddpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
         loss_q.backward()
         q_optimizer.step()
 
-        # Freeze Q-network so you don't waste computational effort 
+        # Freeze Q-network so you don't waste computational effort
         # computing gradients for it during the policy learning step.
         for p in ac.q.parameters():
             p.requires_grad = False
@@ -225,8 +237,10 @@ def ddpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
 
     def get_action(o, noise_scale):
         a = ac.act(torch.as_tensor(o, dtype=torch.float32))
-        a += noise_scale * np.random.randn(act_dim)
-        return np.clip(a, -act_limit, act_limit)
+        a = a * (noise_scale * np.random.randn(act_dim) + 1) # Added by the author
+        return (a / np.sum(a))  # Added by the author
+        # a += noise_scale * np.random.randn(act_dim) Original OpenAI SpinningUp entry
+        # return np.clip(a, -act_limit, act_limit) Original OpenAI SpinningUp entry
 
     def test_agent():
         for j in range(num_test_episodes):
@@ -245,10 +259,12 @@ def ddpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
 
     # Main loop: collect experience in env and update/log each epoch
     for t in range(total_steps):
-        
-        # Until start_steps have elapsed, randomly sample actions
-        # from a uniform distribution for better exploration. Afterwards, 
-        # use the learned policy (with some noise, via act_noise). 
+
+        """
+        Until start_steps have elapsed, randomly sample actions
+        from a uniform distribution for better exploration. Afterwards,
+        use the learned policy (with some noise, via act_noise).
+        """
         if t > start_steps:
             a = get_action(o, act_noise)
         else:
@@ -267,7 +283,7 @@ def ddpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
         # Store experience to replay buffer
         replay_buffer.store(o, a, r, o2, d)
 
-        # Super critical, easy to overlook step: make sure to update 
+        # Super critical, easy to overlook step: make sure to update
         # most recent observation!
         o = o2
 
@@ -309,19 +325,22 @@ def ddpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env', type=str, default='HalfCheetah-v2')
+    # parser.add_argument('--env', type=str, default='HalfCheetah-v2') Original OpenAI SpinningUp entry
     parser.add_argument('--hid', type=int, default=256)
     parser.add_argument('--l', type=int, default=2)
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--seed', '-s', type=int, default=0)
     parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--time_horizon', type = int, default = 80) # Added by the author
+    parser.add_argument('--discount_rate', type = float, default = 0.06) # Added by the author
     parser.add_argument('--exp_name', type=str, default='ddpg')
     args = parser.parse_args()
 
     from spinup.utils.run_utils import setup_logger_kwargs
     logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed)
 
-    ddpg(lambda : gym.make(args.env), actor_critic=core.MLPActorCritic,
-         ac_kwargs=dict(hidden_sizes=[args.hid]*args.l), 
+    ddpg(env_fn = core.ALMEnv, actor_critic=core.MLPActorCritic,
+         ac_kwargs=dict(hidden_sizes=[args.hid]*args.l),
          gamma=args.gamma, seed=args.seed, epochs=args.epochs,
-         logger_kwargs=logger_kwargs)
+         logger_kwargs=logger_kwargs, time_horizon = args.time_horizon,
+         discount_rate = args.discount_rate)
